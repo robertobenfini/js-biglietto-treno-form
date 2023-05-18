8647fb3db4db04fca0394041214a1f6586dcf5f7
robertobenfini <roberto.benfini@gmail.com>
2023-05-18 18:49:54 +0200

    fine-esercizio

--- js/script.js
+++ js/script.js
@@ -4,29 +4,31 @@ const prezzo_km=0.21;
 let button = document.getElementById('genera');
 button.addEventListener('click', function(){
     
+    //input nome e cognome
+    let input_nome = (document.getElementById('nome').value);
+    document.getElementById('nome_pass').innerHTML = input_nome;
+
     //input eta
-    let input_eta = parseInt (document.getElementById('eta').value);
-    console.log(input_eta)
+    let input_eta = (document.getElementById('eta').value);
     
     //input km da percorrere
-    let input_km = parseInt (document.getElementById('km').value);
-    console.log(input_km)    
+    let input_km = parseInt (document.getElementById('km').value);  
 
     //prezzo minorenni
-    if (input_eta < 18) {
+    if (input_eta === '2') {
         let prezzo_minorenni = (input_km * prezzo_km) - ((input_km * prezzo_km) * 20 / 100);
-        console.log(prezzo_minorenni)
+        document.getElementById('prezzo_finale').innerHTML = ((prezzo_minorenni).toFixed(2))+'€';
     }
 
     //prezzo over 65
-    else if (input_eta > 65) {
+    else if (input_eta === '3' ) {
         let prezzo_65 = (input_km * prezzo_km) - ((input_km * prezzo_km) * 40 / 100);
-        console.log(prezzo_65)
+        document.getElementById('prezzo_finale').innerHTML = ((prezzo_65).toFixed(2))+'€';
     }
 
     //prezzo pieno
     else { 
         let prezzo_biglietto_intero = input_km * prezzo_km;
-        console.log(prezzo_biglietto_intero)
+        document.getElementById('prezzo_finale').innerHTML = ((prezzo_biglietto_intero).toFixed(2))+'€';
     }
 })
