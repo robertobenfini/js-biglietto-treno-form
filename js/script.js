1b5ae9ca42069a51388fbdcaf3a2ea2e1f268c0b
robertobenfini <roberto.benfini@gmail.com>
2023-05-19 10:48:08 +0200

    ottimizzazione-script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //prezzo al km
 const prezzo_km=0.21;
+
 //bottone genera
 let button = document.getElementById('genera');
 button.addEventListener('click', function(){
@@ -9,20 +10,20 @@ button.addEventListener('click', function(){
     document.getElementById('nome_pass').innerHTML = input_nome;
 
     //input eta
-    let input_eta = (document.getElementById('eta').value);
+    let input_eta = parseInt (document.getElementById('eta').value);
     
     //input km da percorrere
     let input_km = parseInt (document.getElementById('km').value);  
 
     //prezzo minorenni
-    if (input_eta === '2') {
-        let prezzo_minorenni = (input_km * prezzo_km) - ((input_km * prezzo_km) * 20 / 100);
+    if (input_eta === 2) {
+        let prezzo_minorenni = input_km * prezzo_km * 0.8;
         document.getElementById('prezzo_finale').innerHTML = ((prezzo_minorenni).toFixed(2))+'€';
     }
 
     //prezzo over 65
-    else if (input_eta === '3' ) {
-        let prezzo_65 = (input_km * prezzo_km) - ((input_km * prezzo_km) * 40 / 100);
+    else if (input_eta === 3) {
+        let prezzo_65 = input_km * prezzo_km * 0.6;
         document.getElementById('prezzo_finale').innerHTML = ((prezzo_65).toFixed(2))+'€';
     }
 
